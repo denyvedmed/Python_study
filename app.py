8ee1db906e8536c256acaae5e425a8af5af160c9
Vedmedenko Denys <denysvedmedenko1996@gmail.com>
2022-07-09 11:47:41 +0300

    Trying

---
+++ app.py
@@ -0,0 +1,31 @@
+# # task1
+a1 = max(22, 36)
+print(a1)
+
+a = int(input("Number1: "))
+b = int(input("Number2: "))
+print(max(a, b))
+#
+# # task2
+year = int(input("Year"))
+if year % 4 == 0:
+    print("1 Leap Year")
+elif year % 100 == 0:
+    print("2 No Leap Year")
+elif year % 400 == 0:
+    print("3 Leap Year")
+else:
+    print("4 No Leap Year")
+
+# # task3
+# # координаты Туры
+x1 = int(input("x1"))
+y1 = int(input("y1"))
+
+# # координаты второй Фигуры
+x2 = int(input("x2"))
+y2 = int(input("y2"))
+if x1 == x2 or y1 == y2: print("Beated")
+else: print("Not Beated")
+
+# task4
